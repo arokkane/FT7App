759e610d21b59d3973a06067c616b7dbff58276a
August Rokkanen <arokkane1@gmail.com>
2022-04-28 10:08:22 +0300

    data input improvements

--- website/__init__.py
+++ website/__init__.py
@@ -10,7 +10,7 @@ db = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path='/static')
     app.config.from_object('website.config.DevelopmentConfig')
     db.init_app(app)
     
